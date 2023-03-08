32d4b94c260926402a0aee3ec56f3e2f756877f9
Behon Baker <behon.baker@yahoo.com>
2023-03-07 19:29:40 -0500

    chore: remove sitemap & add pwa support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 const title = "Bloggy";
+const shortTitle = "Bloggy";
 const description = "A blog app built with Nuxt3 + TailwindCSS + Contentv2";
-
+const image = "https://content2-nuxt3.behonbaker.com/icon.png";
+const url = "https://content2-nuxt3.behonbaker.com/";
 export default defineNuxtConfig({
   postcss: {
     plugins: {
@@ -18,30 +20,12 @@ export default defineNuxtConfig({
     "@nuxt/content",
     "nuxt-headlessui",
     "@vueuse/nuxt",
+    "@kevinmarrec/nuxt-pwa",
   ],
   googleFonts: {
     families: {
       Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
       "DM Serif Text": [400],
-    },
-  },
-
-  runtimeConfig: {
-    public: {
-      SITE_NAME: process.env.SITE_NAME,
-    },
-  },
-
-  nitro: {
-    prerender: {
-      routes: ["/sitemap.xml"],
-    },
-  },
-
-  app: {
-    head: {
-      title,
-      titleTemplate: `%s - ${title}`,
     },
   },
 
@@ -60,6 +44,91 @@ export default defineNuxtConfig({
         // Theme used if `html.dark`
         dark: "one-dark-pro",
       },
+    },
+  },
+
+  app: {
+    head: {
+      title,
+      titleTemplate: `%s - ${title}`,
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "canonical",
+          href: url,
+        },
+      ],
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: description,
+        },
+        { property: "og:site_name", content: title },
+        { hid: "og:type", property: "og:type", content: "website" },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: url,
+        },
+        {
+          hid: "og:image:secure_url",
+          property: "og:image:secure_url",
+          content: image,
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: title,
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: description,
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: image,
+        },
+        //Twitter
+        { name: "twitter:card", content: "summary_large_image" },
+        {
+          hid: "twitter:url",
+          name: "twitter:url",
+          content: url,
+        },
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: title,
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: description,
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: image,
+        },
+      ],
+    },
+  },
+
+  pwa: {
+    meta: {
+      name: title,
+      author: "Behon Baker",
+      theme_color: "#10b981",
+      description: description,
+    },
+    manifest: {
+      name: title,
+      short_name: shortTitle,
+      theme_color: "#10b981",
+      description: description,
     },
   },
 });
